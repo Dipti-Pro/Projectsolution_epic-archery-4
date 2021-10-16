52637a198feaff3853edc7eb992bf465dc31e776
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-10-16 15:52:34 +0530

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -9,7 +9,9 @@ class PlayerArrow {
       this.width = width;
       this.height = height;
       this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-      this.image = loadImage("./assets/arrow.png");
+        //make image here
+        
+      
       this.trajectory = [];
       World.add(world, this.body);
     }
@@ -32,12 +34,14 @@ class PlayerArrow {
 
         if (this.body.velocity.x > 0 && this.body.position.x > 400) {
           var position = [this.body.position.x, this.body.position.y];
-          this.trajectory.push(position);
+            //push here the trajectory
+          
         }
     
         for (var i = 0; i < this.trajectory.length; i++) {
           fill("white");
-          ellipse(this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+            //make ellipse here
+          
         }
     
     }
